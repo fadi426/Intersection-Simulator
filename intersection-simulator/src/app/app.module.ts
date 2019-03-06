56a5d3c895b2a5e60d71b04754e77a8a2fa3ae8e
BartdeJong <bart.dejong375@gmail.com>
2019-03-06 20:47:31 +0100

    Added paho mqqt
    Using paho to connect to mqqt server by kleis. Toggle light if message is received from mqtt broker. Added sensor and send value to mqtt broker.

--- intersection-simulator/src/app/app.module.ts
+++ intersection-simulator/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { StompService } from 'ng2-stomp-service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldComponent } from './world/world.component';
@@ -14,7 +13,6 @@ import { WorldComponent } from './world/world.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [StompService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- intersection-simulator/src/app/world/world.component.ts
+++ intersection-simulator/src/app/world/world.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
-
+import { Paho } from 'ng2-mqtt/mqttws31';
 
 @Component({
   selector: 'app-world',
@@ -21,7 +21,11 @@ var groundGeometry, groundMaterial, groundMesh;
 var roadGeometry, roadMaterial, roadMesh;
 var carGeometry, carMaterial, carMesh;
 var trafficLightGeometry, trafficLightMaterial, trafficLightMesh;
+var sensorGeometry, sensorMaterial, sensorMesh;
+var stopLineGeometry, stopLineMaterial, stopLineMesh;
 var trafficLightMode = 0;
+var sensorCurrent = 0;
+var connected = false;
 
 var clock = new THREE.Clock(true);
 
@@ -66,6 +70,20 @@ function init() {
   scene.add(trafficLightMesh);
   trafficLightMesh.position.set(0, 0, 0.1);
 
+  //TrafficLight
+  sensorGeometry = new THREE.BoxGeometry(0.02, 0.02, 0.02);
+  sensorMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+  sensorMesh = new THREE.Mesh(sensorGeometry, sensorMaterial);
+  scene.add(sensorMesh);
+  sensorMesh.position.set(0.20, 0, 0);
+
+  //TrafficLight
+  stopLineGeometry = new THREE.BoxGeometry(0.015, 0.1, 0.02);
+  stopLineMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  stopLineMesh = new THREE.Mesh(stopLineGeometry, stopLineMaterial);
+  scene.add(stopLineMesh);
+  stopLineMesh.position.set(0.14, 0, 0);
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -78,26 +96,78 @@ function animate() {
 
   renderer.render(scene, camera);
 
-  if((Math.abs(carMesh.position.x - trafficLightMesh.position.x) > 0.2) || trafficLightMode == 0 ){
-    carMesh.translateX(-0.01);  
-  }  
+  if (connected) {
+    if ((Math.abs(carMesh.position.x - sensorMesh.position.x)) < 0.1 && sensorCurrent != 1) {
+      sendMessage("1");
+      sensorCurrent = 1;
+    }
+    if((Math.abs(carMesh.position.x - sensorMesh.position.x)) >= 0.1 && sensorCurrent != 0){
+      if (sensorCurrent != 0) {
+        sendMessage("0");
+        sensorCurrent = 0;
+      }
+    }
+  }
+
+  if ((Math.abs(carMesh.position.x - trafficLightMesh.position.x) > 0.2) || trafficLightMode == 0) {
+    carMesh.translateX(-0.01);
+  }
 
   if (carMesh.position.x < -1) {
     carMesh.position.x = 0.9;
   }
 
-  if (clock.getElapsedTime() > 10) {
-    setMode(1);
-    trafficLightMesh.color = 0xff0000;
-  }
-
-  if(trafficLightMode == 0){
+  if (trafficLightMode == 0) {
     trafficLightMesh.material.color.setHex(0x00ff00);
   }
-  else{
+  if(trafficLightMode == 1) {
+    trafficLightMesh.material.color.setHex(0xffa500);
+  }
+  if(trafficLightMode == 2) {
     trafficLightMesh.material.color.setHex(0xff0000);
   }
-  // console.log(clock.getElapsedTime().toString());
-  // console.log(trafficLightMode);
+}
 
+// Create a client instance
+var client = new Paho.MQTT.Client("wss://broker.0f.nl:8084/", "clientId");
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({ onSuccess: onConnect });
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  var subscribeOptions = {
+    qos: 0,  // QoS
+    invocationContext: { foo: true },  // Passed to success / failure callback
+  };
+
+  console.log("onConnect");
+  client.subscribe("8/motor_vehicle/1/light/1", subscribeOptions);
+  sendMessage("1");
+  connected = true;
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+  }
+}
+
+function sendMessage(messageText) {
+  var message = new Paho.MQTT.Message(messageText);
+  message.destinationName = "8/motor_vehicle/1/sensor/1";
+  client.send(message);
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:" + message.payloadString);
+  setMode(Number(message.payloadString));
 }
