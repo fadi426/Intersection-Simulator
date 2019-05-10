252ccd9ff04ddc79543f2b5b4fd5a32cfd291bb1
BartdeJong <bart.dejong375@gmail.com>
2019-05-10 09:42:23 +0200

    Changed initial state of gridge lights

--- intersection-simulator/src/app/trafficLight/traffic-light.ts
+++ intersection-simulator/src/app/trafficLight/traffic-light.ts
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 export class TrafficLight {
     private _mesh : any;
-    private _mode : number;
+    // private _mode : number;
 
-    constructor(private _x : number, private _y : number, private _z : number, private _id : number, private groupId, private group : string, private _rotation : number){
+    constructor(private _x : number, private _y : number, private _z : number, private _id : number, private groupId, private group : string, private _rotation : number, private _mode: number){
         let geometry = new THREE.BoxGeometry(0.015, 0.1, 0.02);
         let material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
         this._mesh = new THREE.Mesh(geometry, material);
         this._mesh.position.set(this._x, this._y, this._z);
-        this._mode = 0;
+        // this._mode = 0;
         this._mesh.rotation.z = this._rotation;
     }
 
--- intersection-simulator/src/app/world/world.component.ts
+++ intersection-simulator/src/app/world/world.component.ts
@@ -85,64 +85,64 @@ function init() {
   // scene.add(road2.getMesh);
 
   //TrafficLights Car
-  trafficLightArr.push(new TrafficLight(-0.35, 0.70, 0, 1, 1, "motor_vehicle", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.21, 0.70, 0, 1, 2, "motor_vehicle", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.07, 0.70, 0, 1, 3, "motor_vehicle", 1.58));
+  trafficLightArr.push(new TrafficLight(-0.35, 0.70, 0, 1, 1, "motor_vehicle", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.21, 0.70, 0, 1, 2, "motor_vehicle", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.07, 0.70, 0, 1, 3, "motor_vehicle", 1.58, 0));
 
-  trafficLightArr.push(new TrafficLight(0.70, 0.35, 0, 1, 4, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(0.70, 0.21, 0, 1, 5, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(0.70, 0.07, 0, 2, 5, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(0.70, -0.07, 0, 1, 6, "motor_vehicle", 0));
+  trafficLightArr.push(new TrafficLight(0.70, 0.35, 0, 1, 4, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.70, 0.21, 0, 1, 5, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.70, 0.07, 0, 2, 5, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.70, -0.07, 0, 1, 6, "motor_vehicle", 0, 0));
 
-  trafficLightArr.push(new TrafficLight(0.35, -0.98, 0, 1, 7, "motor_vehicle", 1.58));
-  trafficLightArr.push(new TrafficLight(0.21, -0.98, 0, 2, 7, "motor_vehicle", 1.58));
-  trafficLightArr.push(new TrafficLight(0.07, -0.98, 0, 1, 8, "motor_vehicle", 1.58));
+  trafficLightArr.push(new TrafficLight(0.35, -0.98, 0, 1, 7, "motor_vehicle", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(0.21, -0.98, 0, 2, 7, "motor_vehicle", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(0.07, -0.98, 0, 1, 8, "motor_vehicle", 1.58, 0));
 
-  trafficLightArr.push(new TrafficLight(-0.70, -0.49, 0, 1, 9, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(-0.70, -0.35, 0, 1, 10, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(-0.70, -0.21, 0, 2, 10, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(-0.70, -0.07, 0, 1, 11, "motor_vehicle", 0));
+  trafficLightArr.push(new TrafficLight(-0.70, -0.49, 0, 1, 9, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.70, -0.35, 0, 1, 10, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.70, -0.21, 0, 2, 10, "motor_vehicle", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.70, -0.07, 0, 1, 11, "motor_vehicle", 0, 0));
 
-  trafficLightArr.push(new TrafficLight(0.40, -0.91, 0, 1, 12, "motor_vehicle", 0));
+  trafficLightArr.push(new TrafficLight(0.40, -0.91, 0, 1, 12, "motor_vehicle", 0, 0));
 
-  trafficLightArr.push(new TrafficLight(9.6, -0.07, 0, 1, 13, "motor_vehicle", 0));
-  trafficLightArr.push(new TrafficLight(9.0, -0.49, 0, 2, 13, "motor_vehicle", 0));
+  trafficLightArr.push(new TrafficLight(9.6, -0.07, 0, 1, 13, "motor_vehicle", 0, 2));
+  trafficLightArr.push(new TrafficLight(9.0, -0.49, 0, 2, 13, "motor_vehicle", 0, 2));
 
   //TrafficLights Cycle
-  trafficLightArr.push(new TrafficLight(0.40, 0.49, 0, 1, 1, "cycle", 0));
-  trafficLightArr.push(new TrafficLight(0.49, -0.56, 0, 1, 2, "cycle", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.42, -0.63, 0, 1, 3, "cycle", 0));
-  trafficLightArr.push(new TrafficLight(-0.49, 0.42, 0, 1, 4, "cycle", 1.58));
+  trafficLightArr.push(new TrafficLight(0.40, 0.49, 0, 1, 1, "cycle", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.49, -0.56, 0, 1, 2, "cycle", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.42, -0.63, 0, 1, 3, "cycle", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.49, 0.42, 0, 1, 4, "cycle", 1.58, 0));
 
-  trafficLightArr.push(new TrafficLight(9.6, 0.07, 0, 1, 5, "cycle", 0));
-  trafficLightArr.push(new TrafficLight(9.0, -0.63, 0, 2, 5, "cycle", 0));
+  trafficLightArr.push(new TrafficLight(9.6, 0.07, 0, 1, 5, "cycle", 0, 2));
+  trafficLightArr.push(new TrafficLight(9.0, -0.63, 0, 2, 5, "cycle", 0, 2));
 
   //TrafficLights Foot
-  trafficLightArr.push(new TrafficLight(-0.42, 0.63, 0, 1, 1, "foot", 0));
-  trafficLightArr.push(new TrafficLight(0.14, 0.63, 0, 1, 2, "foot", 0));
-  trafficLightArr.push(new TrafficLight(0.0, 0.63, 0, 2, 1, "foot", 0));
-  trafficLightArr.push(new TrafficLight(0.40, 0.63, 0, 2, 2, "foot", 0));
+  trafficLightArr.push(new TrafficLight(-0.42, 0.63, 0, 1, 1, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.14, 0.63, 0, 1, 2, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.0, 0.63, 0, 2, 1, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.40, 0.63, 0, 2, 2, "foot", 0, 0));
 
-  trafficLightArr.push(new TrafficLight(0.63, 0.42, 0, 1, 3, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(0.63, -0.28, 0, 1, 4, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(0.63, -0.14, 0, 2, 3, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(0.63, -0.56, 0, 2, 4, "foot", 1.58));
+  trafficLightArr.push(new TrafficLight(0.63, 0.42, 0, 1, 3, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(0.63, -0.28, 0, 1, 4, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(0.63, -0.14, 0, 2, 3, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(0.63, -0.56, 0, 2, 4, "foot", 1.58, 0));
 
-  trafficLightArr.push(new TrafficLight(0.40, -0.77, 0, 1, 5, "foot", 0));
-  trafficLightArr.push(new TrafficLight(-0.14, -0.77, 0, 1, 6, "foot", 0));
-  trafficLightArr.push(new TrafficLight(0.0, -0.77, 0, 2, 5, "foot", 0));
-  trafficLightArr.push(new TrafficLight(-0.42, -0.77, 0, 2, 6, "foot", 0));
+  trafficLightArr.push(new TrafficLight(0.40, -0.77, 0, 1, 5, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.14, -0.77, 0, 1, 6, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(0.0, -0.77, 0, 2, 5, "foot", 0, 0));
+  trafficLightArr.push(new TrafficLight(-0.42, -0.77, 0, 2, 6, "foot", 0, 0));
 
-  trafficLightArr.push(new TrafficLight(-0.63, -0.56, 0, 1, 7, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.63, 0.14, 0, 1, 8, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.63, 0.0, 0, 2, 7, "foot", 1.58));
-  trafficLightArr.push(new TrafficLight(-0.63, 0.42, 0, 2, 8, "foot", 1.58));
+  trafficLightArr.push(new TrafficLight(-0.63, -0.56, 0, 1, 7, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.63, 0.14, 0, 1, 8, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.63, 0.0, 0, 2, 7, "foot", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(-0.63, 0.42, 0, 2, 8, "foot", 1.58, 0));
 
-  trafficLightArr.push(new TrafficLight(9.6, 0.21, 0, 1, 9, "foot", 0));
-  trafficLightArr.push(new TrafficLight(9.0, -0.77, 0, 2, 9, "foot", 0));
+  trafficLightArr.push(new TrafficLight(9.6, 0.21, 0, 1, 9, "foot", 0, 2));
+  trafficLightArr.push(new TrafficLight(9.0, -0.77, 0, 2, 9, "foot", 0, 2));
 
-  trafficLightArr.push(new TrafficLight(9.4, -0.91, 0, 1, 1, "vessel", 1.58));
-  trafficLightArr.push(new TrafficLight(9.2, 0.35, 0, 1, 2, "vessel", 1.58));
+  trafficLightArr.push(new TrafficLight(9.4, -0.91, 0, 1, 1, "vessel", 1.58, 0));
+  trafficLightArr.push(new TrafficLight(9.2, 0.35, 0, 1, 2, "vessel", 1.58, 0));
 
   trafficLightArr.forEach(trafficLight => {
     scene.add(trafficLight.getMesh);
